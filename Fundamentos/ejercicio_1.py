e963e78f68930b406b8b0e3667270994b0a4f100
Angelhorlop <167713157+Angelhorlop@users.noreply.github.com>
2024-04-29 09:54:43 +0200

    ok

--- Fundamentos/ejercicio_1.py
+++ Fundamentos/ejercicio_1.py
@@ -1,7 +1,10 @@
-
+#Dados dos números, escriba un código Python para encontrar el mínimo de estos dos números
+a = int(input("Escribe un numero entero :"))
+b = int(input("Escribe el segundo numero entero:"))
 def minimo (a,b):
     if a < b :
         return (a)
     else :
         return (b)
-print(minimo(-1,-4))
+print(minimo(a,b))
+
